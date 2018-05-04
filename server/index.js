dde6209d0476fc452668ad76d2c808e827c0a418
Matthew Mullen <31232144+mattman240@users.noreply.github.com>
2018-05-04 15:08:40 -0700

    Update index.js

--- server/index.js
+++ server/index.js
@@ -5,16 +5,6 @@ var app = express();
 
 app.use(express.static(__dirname + '/../src/dist'));
 
-// app.get('/items', function (req, res) {
-//   items.selectAll(function(err, data) {
-//     if(err) {
-//       res.sendStatus(500);
-//     } else {
-//       res.json(data);
-//     }
-//   });
-// });
-
 app.listen(3000, function() {
   console.log('listening on port 3000!');
 });
